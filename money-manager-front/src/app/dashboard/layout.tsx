599dcd9b104a43d16f070efc0fec8633ec2d8506
jeferreirajf <jeferreirajf@gmail.com>
2023-07-25 15:42:32 -0300

    Add dashboard and dashboard headder

---
+++ money-manager-front/src/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import { BadgeDollarSign } from "lucide-react";
+import React from "react";
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <div className="container flex h-20 rounded-xl items-center m-auto gap-4 shadow-md bg-zinc-800">
+                <BadgeDollarSign className="text-emerald-400" size={48}/>
+                <h1 className="uppercase font-bold text-3xl text-slate-100">Money Manager</h1>
+            </div>
+            {children}
+        </>
+    )
+}
