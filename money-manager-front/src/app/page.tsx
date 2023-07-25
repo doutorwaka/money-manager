b49658751ca6e2addb9a404d4b67be2a65aad2a8
jeferreirajf <jeferreirajf@gmail.com>
2023-07-25 15:08:27 -0300

    Add login title style

--- money-manager-front/src/app/page.tsx
+++ money-manager-front/src/app/page.tsx
@@ -1,7 +1,19 @@
+import { BadgeDollarSign } from "lucide-react";
+
 export default function Home() {
   return (
     <main>
-      <h1>Olá Money Manager!</h1>
+      <div className="flex items-center h-screen">
+
+        <div className="container p-8 max-w-md h-96 rounded-xl bg-gray-50 shadow-md">
+          <span className="flex items-center gap-2">
+            <BadgeDollarSign className="text-slate-500" size={48} />
+            <h1 className="uppercase text-slate-600 font-bold text-xl">Money Manager</h1>
+          </span>
+          
+        </div>
+
+      </div>
     </main>
   )
 }
