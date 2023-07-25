c40fd6005517eaedfb87588fce1e03e00439c6f6
jeferreirajf <jeferreirajf@gmail.com>
2023-07-25 15:47:06 -0300

    Add dashboard content container

--- money-manager-front/src/app/dashboard/layout.tsx
+++ money-manager-front/src/app/dashboard/layout.tsx
@@ -5,10 +5,12 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
     return (
         <>
             <div className="container flex h-20 rounded-xl items-center m-auto gap-4 shadow-md bg-zinc-800">
-                <BadgeDollarSign className="text-emerald-400" size={48}/>
+                <BadgeDollarSign className="text-emerald-400" size={48} />
                 <h1 className="uppercase font-bold text-3xl text-slate-100">Money Manager</h1>
             </div>
-            {children}
+            <div className="container items-center gap-4 w-md m-auto mt-2 rounded-xl shadow-md h-40 bg-slate-50">
+                {children}
+            </div>
         </>
     )
 }
