abb382ca530c806fef846e069ccc5ecc0227b2ec
jeferreirajf <jeferreirajf@gmail.com>
2023-08-01 10:53:44 -0300

    Add Spring, Spring JPA and ActivityJpaEntity

--- money-manager-back/app/src/main/java/money/manager/App.java
+++ money-manager-back/app/src/main/java/money/manager/App.java
@@ -3,8 +3,24 @@
  */
 package money.manager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import money.manager.configuration.WebserverConfiguration;
+
+@SpringBootApplication
 public class App {
+
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        LOG.info("Starting money.manager... ");
+
+        SpringApplication.run(WebserverConfiguration.class, args);
+        
+        LOG.info("Ending of starting money.manager... ");
     }
 }
---
+++ money-manager-back/app/src/main/java/money/manager/repository/activity/ActivityJpaGateway.java
@@ -0,0 +1,28 @@
+package money.manager.repository.activity;
+
+import java.util.List;
+
+import money.manager.domain.activity.Activity;
+import money.manager.domain.gateway.ActivityGateway;
+
+public class ActivityJpaGateway implements ActivityGateway {
+
+    @Override
+    public void create(Activity anActivity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    }
+
+    @Override
+    public void delete(String anId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public List<Activity> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+}
