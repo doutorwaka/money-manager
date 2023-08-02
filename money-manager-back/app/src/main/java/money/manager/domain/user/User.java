fb51c50aa4d9952e7d828abe4c3ae7541003446c
jeferreirajf <jeferreirajf@gmail.com>
2023-08-02 10:32:34 -0300

    Add domain user

---
+++ money-manager-back/app/src/main/java/money/manager/domain/user/User.java
@@ -0,0 +1,31 @@
+package money.manager.domain.user;
+
+public class User {
+
+    private String email;
+    private String password;
+
+    private User(final String anEmail, final String aPassword) {
+        this.email = anEmail;
+        this.password = aPassword;
+    }
+
+    public static User with(final String anEmail, final String aPassword) {
+        return new User(anEmail, aPassword);
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+}
