ead24d3b7e803c9856c20e7c839fb2c62e17222d
jeferreirajf <jeferreirajf@gmail.com>
2023-08-04 10:04:56 -0300

    Add UserDetails

--- money-manager-back/app/src/main/java/money/manager/domain/user/User.java
+++ money-manager-back/app/src/main/java/money/manager/domain/user/User.java
@@ -1,6 +1,13 @@
 package money.manager.domain.user;
 
-public class User {
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails {
 
     private String email;
     private String password;
@@ -26,5 +33,35 @@ public class User {
      */
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
