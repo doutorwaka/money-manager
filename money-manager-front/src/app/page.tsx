14b19a43e01df44509360e90b5ac7bdaacc6fae4
jeferreirajf <jeferreirajf@gmail.com>
2023-08-07 11:06:42 -0300

    Add zod, react-hook-form and shadcn/Form

--- money-manager-front/src/app/page.tsx
+++ money-manager-front/src/app/page.tsx
@@ -1,23 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { BadgeDollarSign } from "lucide-react";
+import { LoginForm } from "@/components/main/login-form";
 
 export default function Home() {
   return (
     <main>
-      <div className="flex pb-60 items-center h-screen">
-
-        <div className="container space-y-2 p-8 max-w-md rounded-xl bg-gray-50 shadow-md">
-          <span className="flex items-center gap-2">
-            <BadgeDollarSign className="text-slate-500" size={48} />
-            <h1 className="uppercase text-slate-600 font-bold text-xl">Money Manager</h1>
-          </span>
-          <Input type="email" placeholder="Digite seu e-mail"/>
-          <Input type="password" placeholder="Digite sua senha"/>
-          <Button>Enviar</Button>
-        </div>
-
-      </div>
+      <LoginForm />
     </main>
   )
 }
---
+++ money-manager-front/src/components/main/login-form/index.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { BadgeDollarSign } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+
+const loginFormSchema = z.object({
+    email: z.string().email({ message: "E-mail inválido" }),
+    password: z.string().min(1, { message: "Senha inválida" })
+});
+
+type LoginFormType = z.infer<typeof loginFormSchema>;
+
+export function LoginForm() {
+
+    const loginForm = useForm<LoginFormType>({
+        resolver: zodResolver(loginFormSchema),
+        defaultValues: {
+            email: "",
+            password: ""
+        }
+    });
+
+    function handleLoginSubmit({ email, password }: LoginFormType) {
+        console.log(email, password);
+    }
+
+    return (
+        <>
+            <div className="flex pb-60 items-center h-screen">
+                <div className="container space-y-2 p-8 max-w-md rounded-xl bg-gray-50 shadow-md">
+                    <span className="flex items-center gap-2">
+                        <BadgeDollarSign className="text-slate-500" size={48} />
+                        <h1 className="uppercase text-slate-600 font-bold text-xl">Money Manager</h1>
+                    </span>
+
+                    <Form {...loginForm}>
+                        <form onSubmit={loginForm.handleSubmit(handleLoginSubmit)}>
+                            <FormField
+                                control={loginForm.control}
+                                name="email"
+                                render={({ field }) => {
+                                    return (
+                                        <FormItem>
+                                            <FormControl>
+                                                <Input type="text" placeholder="Digite seu e-mail" {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )
+                                }}
+                            />
+
+                            <FormField
+                                control={loginForm.control}
+                                name="password"
+                                render={({ field }) => {
+                                    return (<FormItem>
+                                        <FormControl>
+                                            <Input type="password" placeholder="Digite sua senha" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>)
+                                }}
+                            />
+
+                            <Button type="submit">Enviar</Button>
+                        </form>
+                    </Form>
+
+                </div>
+
+            </div>
+        </>
+    )
+}
