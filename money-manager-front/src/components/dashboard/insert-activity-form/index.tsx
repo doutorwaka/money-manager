5de7f2e4a310b58a2fac97757394939f2ba986f6
jeferreirajf <jeferreirajf@gmail.com>
2023-08-15 15:43:55 -0300

    Fix dashboard insert activity form part 2

--- money-manager-front/src/components/dashboard/insert-activity-form/index.tsx
+++ money-manager-front/src/components/dashboard/insert-activity-form/index.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
-import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select";
@@ -21,7 +21,7 @@ enum ActivityType {
 const insertFormSchema = z.object({
     date: z.date(),
     description: z.string(),
-    value: z.number(),
+    value: z.coerce.number(),
     type: z.nativeEnum(ActivityType)
 });
 
@@ -39,54 +39,101 @@ export function InsertActivityForm() {
         }
     });
 
+    async function onInsertFormSubmit(date: InsertFormType) {
+        console.log(date);
+    }
+
     return (
         <div className="flex space-x-2 p-8">
             <Form {...insertForm}>
-                <FormField
-                    control={insertForm.control}
-                    name="date"
-                    render={({ field }) => {
-                        return (
-                            <FormItem className="max-w-[10rem]">
-                                <Popover>
-                                    <PopoverTrigger asChild>
-                                        <Button
-                                            variant={"outline"}
-                                            className={cn(
-                                                "w-max justify-start text-left font-normal",
-                                                !field.value && "text-muted-foreground"
-                                            )}
-                                        >
-                                            <CalendarIcon className="mr-2 h-4 w-4" />
-                                            {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
-                                        </Button>
-                                    </PopoverTrigger>
-                                    <PopoverContent className="w-auto p-0">
-                                        <Calendar
-                                            mode="single"
-                                            selected={field.value}
-                                            onSelect={field.onChange}
-                                            initialFocus
-                                        />
-                                    </PopoverContent>
-                                </Popover>
-                            </FormItem>
-                        )
-                    }}
-                />
+                <form onSubmit={insertForm.handleSubmit(onInsertFormSubmit)}>
+                    <FormField
+                        control={insertForm.control}
+                        name="date"
+                        render={({ field }) => {
+                            return (
+                                <FormItem className="w-max">
+                                    <Popover>
+                                        <PopoverTrigger asChild>
+                                            <Button
+                                                variant={"outline"}
+                                                className={cn(
+                                                    "w-max justify-start text-left font-normal",
+                                                    !field.value && "text-muted-foreground"
+                                                )}
+                                            >
+                                                <CalendarIcon className="mr-2 h-4 w-4" />
+                                                {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+                                            </Button>
+                                        </PopoverTrigger>
+                                        <PopoverContent className="w-auto p-0">
+                                            <Calendar
+                                                mode="single"
+                                                selected={field.value}
+                                                onSelect={field.onChange}
+                                                initialFocus
+                                            />
+                                        </PopoverContent>
+                                    </Popover>
+                                </FormItem>
+                            )
+                        }}
+                    />
 
-                <Input type="text" placeholder="Insira a descrição da atividade..." />
-                <Input type="number" className="w-max" placeholder="Digite o valor..." />
-                <Select>
-                    <SelectTrigger className="w-80">
-                        <SelectValue placeholder="Selecione o tipo" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectItem value="0">Entrada</SelectItem>
-                        <SelectItem value="1">Saída</SelectItem>
-                    </SelectContent>
-                </Select>
-                <Button>Incluir</Button>
+                    <FormField
+                        control={insertForm.control}
+                        name="description"
+                        render={({ field }) => {
+                            return (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input type="text" placeholder="Insira a descrição da atividade..." {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )
+                        }}
+                    />
+
+                    <FormField
+                        control={insertForm.control}
+                        name="value"
+                        render={({ field }) => {
+                            return (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input type="number" className="w-max" placeholder="Digite o valor..." {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )
+                        }}
+                    />
+
+                    <FormField
+                        control={insertForm.control}
+                        name="type"
+                        render={({ field }) => {
+                            return (
+                                <FormItem>
+                                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                        <FormControl>
+                                            <SelectTrigger className="w-80">
+                                                <SelectValue placeholder="Selecione o tipo" />
+                                            </SelectTrigger>
+                                        </FormControl>
+                                        <SelectContent>
+                                            <SelectItem value={ActivityType.REVENUE}>Entrada</SelectItem>
+                                            <SelectItem value={ActivityType.EXPENSE}>Saída</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                    <FormMessage />
+                                </FormItem>
+                            )
+                        }}
+                    />
+                    <Button type="submit">Incluir</Button>
+                </form>
             </Form>
         </div>
     );
